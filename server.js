c66c84060b9262b0cf7edbaf5d7075c9fb8ac18c
AjayMohan1606 <ajaymudappallur2004@gmail.com>
2026-03-01 20:15:26 +0530

    server.js

--- server.js
+++ server.js
@@ -1,35 +1,11 @@
 const express = require("express");
-const http = require("http");
-const { Server } = require("socket.io");
+const { PeerServer } = require("peer");
 
 const app = express();
-const server = http.createServer(app);
-const io = new Server(server, {
-  cors: {
-    origin: "*"
-  }
+
+const server = app.listen(process.env.PORT || 3000);
+
+PeerServer({
+  port: process.env.PORT || 3000,
+  path: "/peerjs"
 });
-
-io.on("connection", socket => {
-
-  socket.on("join-room", roomId => {
-    socket.join(roomId);
-    socket.to(roomId).emit("user-joined");
-  });
-
-  socket.on("offer", data => {
-    socket.to(data.roomId).emit("offer", data);
-  });
-
-  socket.on("answer", data => {
-    socket.to(data.roomId).emit("answer", data);
-  });
-
-  socket.on("ice-candidate", data => {
-    socket.to(data.roomId).emit("ice-candidate", data);
-  });
-
-});
-
-const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => console.log("Server running"));
